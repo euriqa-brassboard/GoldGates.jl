ade749ba61ca5d4e35f3d5f831534033e0b92653
Yichao Yu <yyc1992@gmail.com>
2025-09-12 10:16:34 -0400

    Serializing and deserializing of ParticipationFactor

--- test/gate_params.jl
+++ test/gate_params.jl
@@ -2,6 +2,7 @@
 
 module GateParams
 
+using GoldGates
 using GoldGates: ParticipationFactor, XXSolution, SysMetadata, Modes,
     SystemParams, GateSolutionSet
 
@@ -21,6 +22,24 @@ using Test
     @test pf[1] == 0.1
     @test pf[2] == 0.9
     @test pf[3] == 0.3
+
+    @test PB.default_values(ParticipationFactor) == (;factors=Float64[])
+    @test PB.field_numbers(ParticipationFactor) == (;factors=1)
+
+    io = IOBuffer()
+    encoder = PB.ProtoEncoder(io)
+    PB.encode(encoder, pf)
+    @test PB._encoded_size(pf) == io.size
+    seekstart(io)
+
+    decoder = PB.ProtoDecoder(io)
+    pf3 = PB.decode(decoder, ParticipationFactor)
+    @test pf.factors == pf3.factors
+
+    js = GoldGates._to_json(pf2)
+    @test js === pf2.factors
+    pf4 = GoldGates._load_json(js, ParticipationFactor)
+    @test pf4.factors === pf2.factors
 end
 
 end
